d9500154f99e77bc1d20e991fbfe9c41e35df11b
kfconeone <kfconeone@gmail.com>
2022-12-08 16:18:06 +0800

    change to 10s reload

--- app.vue
+++ app.vue
@@ -12,27 +12,20 @@ onMounted(() => {
         player.load("https://3fc23fa3dfd6.us-east-1.playback.live-video.net/api/video/v1/us-east-1.612025208159.channel.uEgiPSFQdImX.m3u8");
         player.play();
 
-        window.playIvsPlayer = onPlay;
-        window.pauseIvsPlayer = onPaused;
         // player.
+        reload();
     }
 });
 
-function onPaused() {
-    console.log("paused");
-    player.pause();
-}
-
-
-function onPlay() {
-    console.log("paused");
+async function reload() {
     player.play();
-}
+    setTimeout(() => {
+        // player.load("https://3fc23fa3dfd6.us-east-1.playback.live-video.net/api/video/v1/us-east-1.612025208159.channel.NRAjrwcZ7JaY.m3u8");
+        console.log("reloading");
+        player.pause();
+        reload();
+    }, 10000);
 
-
-function test() {
-    console.log("test");
-    window.pauseIvsPlayer();
 }
 </script>
 <template>
